5d21f1588fe4a273729556fb1d6079a5f14cf92c
Cedric Gatay <c.gatay@code-troopers.com>
2020-03-11 15:04:24 +0100

    feat(interop): code style
    extracted methods from current test cases.

--- app/src/iosMain/kotlin/sample/SampleIos.kt
+++ app/src/iosMain/kotlin/sample/SampleIos.kt
@@ -3,17 +3,40 @@ import bridge.call_chacha20
 import bridge.say_hello
 import bridge.theme_song_generate
 import kotlinx.cinterop.*
-import libsecp256k1.SECP256K1_EC_COMPRESSED
-import libsecp256k1.secp256k1_ec_pubkey_create
-import libsecp256k1.secp256k1_ec_pubkey_serialize
-import libsecp256k1.secp256k1_pubkey
+import libsecp256k1.*
 import platform.posix.size_tVar
 
 actual class Sample {
     actual fun checkMe() = 7
 
     actual fun callBridge(){
-        val secp256k1ContextCreate = libsecp256k1.secp256k1_context_create(((1 shl 9) + (1 shl 8) + 1).toUInt())
+        callLibSecp256k1()
+        callRustBridgeToChacha20()
+
+    }
+
+    private fun callRustBridgeToChacha20() {
+        println(">>> Entering call bridge")
+        memScoped {
+            val input = "this is kotlin speaking"
+            val sayHello = say_hello(input)?.toKStringFromUtf8()
+            println(">>> Say Hello ${sayHello}")
+        }
+        val themeSongGenerate = theme_song_generate(8.toUByte())?.toKStringFromUtf8()
+        println(">>> toKString ${themeSongGenerate}")
+        val callChacha20 = call_chacha20("hello from kotlin")
+        callChacha20?.apply {
+            for (index in 0..6) {
+                println("${index} - ${this[index]}")
+            }
+        }
+        println("callChacha20 = ${callChacha20}")
+        val readBytes = callChacha20?.readBytes(7)
+        println("readBytes = ${readBytes?.map { it.toUByte() }}")
+    }
+
+    private fun callLibSecp256k1() {
+        val secp256k1ContextCreate = secp256k1_context_create(((1 shl 9) + (1 shl 8) + 1).toUInt())
         memScoped {
             val secp256k1Pubkey = nativeHeap.alloc<secp256k1_pubkey>()
             val ctmp = nativeHeap.allocArray<UByteVar>(32)
@@ -33,7 +56,13 @@ actual class Sample {
             val output = nativeHeap.alloc<secp256k1_pubkey>()
             var length = nativeHeap.alloc<size_tVar>()
             length.value = 33.toULong()
-            secp256k1_ec_pubkey_serialize(secp256k1ContextCreate, output.data, length.ptr , secp256k1Pubkey.ptr, SECP256K1_EC_COMPRESSED)
+            secp256k1_ec_pubkey_serialize(
+                secp256k1ContextCreate,
+                output.data,
+                length.ptr,
+                secp256k1Pubkey.ptr,
+                SECP256K1_EC_COMPRESSED
+            )
             println("output = ${output}")
 
             val dataStr = ArrayList<UByte>()
@@ -42,27 +71,9 @@ actual class Sample {
                 dataStr.add(uByte)
             }
             println(">>>> data ${dataStr}")
-            libsecp256k1.secp256k1_context_destroy(secp256k1ContextCreate!!)
+            secp256k1_context_destroy(secp256k1ContextCreate!!)
 
         }
-        println(">>> Entering call bridge")
-        memScoped {
-            val input = "this is kotlin speaking"
-            val sayHello = say_hello(input)?.toKStringFromUtf8()
-            println(">>> Say Hello ${sayHello}")
-        }
-        val themeSongGenerate = theme_song_generate(8.toUByte())?.toKStringFromUtf8()
-        println(">>> toKString ${themeSongGenerate}")
-        val callChacha20 = call_chacha20("hello from kotlin")
-        callChacha20?.apply {
-            for (index in 0..6) {
-                println("${index} - ${this[index]}")
-            }
-        }
-        println("callChacha20 = ${callChacha20}")
-        val readBytes = callChacha20?.readBytes(7)
-        println("readBytes = ${readBytes?.map { it.toUByte() }}")
-
     }
 }
 
